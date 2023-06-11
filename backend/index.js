fd9559466c845ada7e2959171dcf9fc2d1449625
ehtasham akmal <ehtashamakmal@gmail.com>
2023-06-11 13:10:43 +0500

    GoFood

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express()
+
+const PORT = 5000 
+const db = require('./db');
+db();
+
+app.use((req,res,next)=>{
+res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+res.header(
+    "Access-Control-Allow-Headers",
+    "Origin , X-Requested-With , Content-Type, Accept" 
+);
+next();
+})
+
+
+
+//middleware
+
+
+
+app.use(express.json())
+app.use('/api', require('./Routes/CreateUser'));
+app.use('/api', require('./Routes/DisplayData')); 
+app.use('/api', require('./Routes/OrderData'));
+
+app.get('/', (req,res) => {
+
+    res.send("Hello World!")
+    
+    
+    }) 
+
+
+
+
+app.listen(PORT , () =>  {
+    console.log("Server is running on port "+PORT);
+});
